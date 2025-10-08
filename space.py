9fe011993bcb71e6e2a41559ef2a408983bcf2f9
Jana <jana.maknojia@gmail.com>
2025-10-07 23:00:56 -0500

    Add files via upload

---
+++ space.py
@@ -0,0 +1,69 @@
+from dash import Dash, dcc, html
+from dash.dependencies import Input, Output
+import pandas as pd
+import plotly.express as px
+
+
+spacex_df = pd.read_csv(
+    "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/datasets/spacex_launch_dash.csv"
+)
+
+app = Dash(__name__)
+
+launch_sites = spacex_df['Launch Site'].unique().tolist()
+dropdown_options = [{'label': 'All Sites', 'value': 'ALL'}] + \
+                   [{'label': site, 'value': site} for site in launch_sites]
+
+min_payload = spacex_df['Payload Mass (kg)'].min()
+max_payload = spacex_df['Payload Mass (kg)'].max()
+
+app.layout = html.Div([
+    html.H1("SpaceX Launch Dashboard"),
+
+    dcc.Dropdown(
+        id='site-dropdown',
+        options=dropdown_options,
+        value='ALL',
+        placeholder="Select a Launch Site here",
+        searchable=True
+    ),
+
+    dcc.RangeSlider(
+        id='payload-slider',
+        min=0,
+        max=10000,
+        step=1000,
+        value=[min_payload, max_payload],
+        marks={i: str(i) for i in range(0, 10001, 1000)}
+    ),
+
+    dcc.Graph(id='success-payload-scatter-chart')
+])
+
+@app.callback(
+    Output(component_id='success-payload-scatter-chart', component_property='figure'),
+    Input(component_id='site-dropdown', component_property='value'),
+    Input(component_id='payload-slider', component_property='value')
+)
+def update_scatter(selected_site, payload_range):
+    low, high = payload_range
+    filtered_df = spacex_df[(spacex_df['Payload Mass (kg)'] >= low) & 
+                            (spacex_df['Payload Mass (kg)'] <= high)]
+    
+    if selected_site == 'ALL':
+        fig = px.scatter(
+            filtered_df, x='Payload Mass (kg)', y='class',
+            color='Booster Version Category',
+            title='Payload vs. Outcome for All Sites'
+        )
+    else:
+        site_df = filtered_df[filtered_df['Launch Site'] == selected_site]
+        fig = px.scatter(
+            site_df, x='Payload Mass (kg)', y='class',
+            color='Booster Version Category',
+            title=f'Payload vs. Outcome for site {selected_site}'
+        )
+    return fig
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8051)
